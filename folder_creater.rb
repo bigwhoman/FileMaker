b171c0d2eb48a3923e34312ad47e75c99e7e2c10
Hooman Keshvari <79264715+bigwhoman@users.noreply.github.com>
2022-10-17 13:32:39 +0330

    Update folder_creater.rb

--- folder_creater.rb
+++ folder_creater.rb
@@ -1,23 +1,24 @@
 dir = "../term4"
-newDirName = "halTamrin"
-termNum = 4
-subDirName = "hal"
-numberOfSubDirs = 10
-  Dir.children("../term#{termNum}/").each do |folder|
-    Dir.mkdir("../term#{termNum}/#{folder}/#{newDirName}") unless Dir.children("../term#{termNum}/#{folder}").include? "#{newDirName}"
-    (1..numberOfSubDirs).each do |i|
-      Dir.mkdir("../term#{termNum}/#{folder}/#{newDirName}/#{subDirName}#{i}") unless Dir
-                                                                                  .children("../term#{termNum}/#{folder}/#{newDirName}")
-                                                                                  .include? "#{subDirName}#{i}"
-      File.open("../term#{termNum}/#{folder}/#{newDirName}/#{subDirName}#{i}/notes.txt", "w") do |f|
-        f.write("no notes yet")
-      end
+newDirName = "Slides"
+termNum = 5
+subDirName = "slide"
+base_dir = "H:/Sharif/"
+numberOfSubDirs = 15
+Dir.children("#{base_dir}/term#{termNum}").each do |folder|
+  Dir.mkdir("#{base_dir}/term#{termNum}/#{folder}/#{newDirName}") unless Dir.children("#{base_dir}/term#{termNum}/#{folder}").include? "#{newDirName}"
+  (1..numberOfSubDirs).each do |i|
+    Dir.mkdir("#{base_dir}/term#{termNum}/#{folder}/#{newDirName}/#{subDirName}#{i}") unless Dir
+                                                                                      .children("#{base_dir}/term#{termNum}/#{folder}/#{newDirName}")
+                                                                                      .include? "#{subDirName}#{i}"
+    File.open("#{base_dir}/term#{termNum}/#{folder}/#{newDirName}/#{subDirName}#{i}/notes.txt", "w") do |f|
+      f.write("no notes yet")
     end
-    # Dir.mkdir(dir)
-    # Dir.children(dir).each do |folder|
-    #   Dir.mkdir(dir+"/#{folder}")
-    #   Dir.mkdir(dir+"/"+newDirName+"#{i}") unless Dir.children(dir).include?"#{newDirName}+#{i}"
-    # File.open(dir+"/homework#{i}/deadline.txt","w") do |f|
-    #   f.write("not determined")
-    # end
   end
+  # Dir.mkdir(dir)
+  # Dir.children(dir).each do |folder|
+  #   Dir.mkdir(dir+"/#{folder}")
+  #   Dir.mkdir(dir+"/"+newDirName+"#{i}") unless Dir.children(dir).include?"#{newDirName}+#{i}"
+  # File.open(dir+"/homework#{i}/deadline.txt","w") do |f|
+  #   f.write("not determined")
+  # end
+end
